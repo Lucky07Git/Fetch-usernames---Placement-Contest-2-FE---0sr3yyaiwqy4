9a27379404481052ffb16faea58f2aeef0f9a945
Abhishek Verma <102553122+Lucky07Git@users.noreply.github.com>
2022-10-09 22:43:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,31 @@ import '../styles/App.css';
 import { useState, useEffect } from 'react';
 const App = () => {
 //code here 
- 
+  const [name, setName] = useState("");
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://content.newtonschool.co/v1/pr/main/users/${id}`
+      );
+      const json = await response.json();
+      setName(json.name);
+    };
+    fetchData();
+  }, [id]);
 
+  const handleChange = event => {
+    setId(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (id) {
+      fetchData();
+    } else {
+      alert("Please insert valid id");
+    }
+  };
 
   return (
     <div className="App">
